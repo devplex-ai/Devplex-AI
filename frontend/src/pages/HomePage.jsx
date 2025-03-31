b92e663005c50663ed2bb27269170e840fcbab6e
yashtupkar <yashtupkar6@gmail.com>
2025-03-31 20:02:28 +0530

    adding AppSideBar in HomePage

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -106,7 +106,7 @@ const handleGenerate = async () => {
 
   return (
     <div className="bg-black h-full min-h-screen">
-    
+    <AppSideBar/>
       <Navbar />
       <div className="relative overflow-hidden min-h-screen flex flex-col items-center justify-center text-center px-4 sm:px-6 bg-black text-white">
         {/* Background Glow Effect - Adjusted for mobile */}
