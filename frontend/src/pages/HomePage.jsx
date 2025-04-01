72cb8d41097171b8000dd49c4d0fe19cbd55c6f0
yashtupkar <yashtupkar6@gmail.com>
2025-04-01 10:04:46 +0530

    some changes

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -198,7 +198,7 @@ const VideoModal = () => {
       <div className="bg-white/10 p-6 rounded-lg shadow-lg w-full max-w-lg text-center">
         <h2 className="text-xl font-semibold mb-3 text-white">Generating...</h2>
 
-        <video autoPlay loop muted className="w-40 rounded-full ">
+        <video autoPlay loop muted className="w-full rounded-lg ">
           <source src="/assets/codevideo.mp4" type="video/mp4"  />
           Your browser does not support the video tag.
         </video>
