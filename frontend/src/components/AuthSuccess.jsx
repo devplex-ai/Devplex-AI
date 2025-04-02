63888c5c94d4e5f10522c95d549b153c5b26260f
yashtupkar <yashtupkar6@gmail.com>
2025-04-02 14:37:18 +0530

    error fixing

--- frontend/src/components/AuthSuccess.jsx
+++ frontend/src/components/AuthSuccess.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
-import { setUser } from "../redux/authSlice"; // Import the Redux action
+import { setUser } from "../../redux/authSlice"; // Import the Redux action
 
 const AuthSuccess = () => {
   const navigate = useNavigate();
